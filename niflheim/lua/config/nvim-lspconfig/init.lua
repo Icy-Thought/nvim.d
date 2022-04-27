fab8be5bbde9f79d7e6fe3448011115e73950f3f
Icy-Thought <icy-thought@pm.me>
2022-04-27 10:38:57 +0200

    [🚀]: api keymap -> keymap.set

--- niflheim/lua/config/nvim-lspconfig/init.lua
+++ niflheim/lua/config/nvim-lspconfig/init.lua
@@ -41,31 +41,11 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 end
 
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap(
-    "n",
-    "<space>e",
-    "<cmd>lua vim.diagnostic.open_float()<CR>",
-    opts
-)
-vim.api.nvim_set_keymap(
-    "n",
-    "[d",
-    "<cmd>lua vim.diagnostic.goto_prev()<CR>",
-    opts
-)
-vim.api.nvim_set_keymap(
-    "n",
-    "]d",
-    "<cmd>lua vim.diagnostic.goto_next()<CR>",
-    opts
-)
-vim.api.nvim_set_keymap(
-    "n",
-    "<space>q",
-    "<cmd>lua vim.diagnostic.setloclist()<CR>",
-    opts
-)
+local opts = { noremap = true, silent = true, buffer = bufnr }
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -75,97 +55,24 @@ local on_attach = function(client, bufnr)
 
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "gD",
-        "<cmd>lua vim.lsp.buf.declaration()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "gd",
-        "<cmd>lua vim.lsp.buf.definition()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "K",
-        "<cmd>lua vim.lsp.buf.hover()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "gi",
-        "<cmd>lua vim.lsp.buf.implementation()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<C-k>",
-        "<cmd>lua vim.lsp.buf.signature_help()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<space>wa",
-        "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<space>wr",
-        "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set(
         "n",
         "<space>wl",
-        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+        vim.pretty_print(vim.lsp.buf.list_workspace_folders),
         opts
     )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<space>D",
-        "<cmd>lua vim.lsp.buf.type_definition()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<space>rn",
-        "<cmd>lua vim.lsp.buf.rename()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<space>ca",
-        "<cmd>lua vim.lsp.buf.code_action()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "gr",
-        "<cmd>lua vim.lsp.buf.references()<CR>",
-        opts
-    )
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<space>f",
-        "<cmd>lua vim.lsp.buf.formatting()<CR>",
-        opts
-    )
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, opts)
 end
 
 -- Add additional capabilities supported by nvim-cmp
@@ -174,48 +81,21 @@ local cmp_nvim_lsp = prequire("cmp_nvim_lsp")
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
-local servers = {
-    "bashls",
-    "hls",
-    "pyright",
-    "rnix",
-    "rust_analyzer",
-    "sumneko_lua",
-    "texlab",
-}
+-- Language Server Protocols
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
--- Use a loop to conveniently call 'setup' on multiple servers
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        -- Language Server Protocols
-        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-        settings = {
-            haskell = {
-                formattingProvider = "stylish-haskell",
-            },
-            rnix = {
-                formattingProvider = "nixfmt",
-            },
-            texlab = {
-                build = {
-                    args = {
-                        "-X",
-                        "compile",
-                        "%f",
-                        "--synctex",
-                        "--keep-logs",
-                        "--keep-intermediates",
-                    },
-                    executable = "tectonic",
-                },
-                forwardSearch = {
-                    executable = "zathura",
-                    args = { "--synctex-forward", "%l:1:%f", "%p" },
-                },
-                latexFormatter = "latexindent",
-            },
+lspconfig.hls.setup({
+    on_attach = on_attach,
+    settings = {
+        haskell = {
+            formattingProvider = "stylish-haskell",
         },
-    })
-end
+    },
+})
+
+lspconfig.rnix.setup({
+    on_attach = on_attach,
+    settings = {
+        formattingProvider = "nixfmt",
+    },
+})
--- niflheim/lua/config/toggleterm.nvim/init.lua
+++ niflheim/lua/config/toggleterm.nvim/init.lua
@@ -6,8 +6,8 @@ toggleterm.setup({
             return 15
         elseif term.direction == "vertical" then
             return vim.o.columns * 0.40
-            end
-        end,
+        end
+    end,
     open_mapping = [[<c-\>]],
     hide_numbers = true,
     shade_filetypes = {},
@@ -26,27 +26,27 @@ toggleterm.setup({
         highlights = {
             border = "Normal",
             background = "Normal",
-        }
-    }
+        },
+    },
 })
 
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+    local opts = { noremap = true, buffer = true }
+    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 -- Use fn's at which-key/init
-local Terminal  = prequire("toggleterm.terminal").Terminal
+local Terminal = prequire("toggleterm.terminal").Terminal
 
 local python = Terminal:new({ cmd = "python", hidden = true })
 function _PYTHON_TOGGLE()
-	python:toggle()
+    python:toggle()
 end
