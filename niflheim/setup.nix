124b58e9dab355269e10190a6453f546947c7ecf
Icy-Thought <icy-thought@pm.me>
2022-06-25 12:27:06 +0200

    [𝕵] (niflheim): alejandra needs proper nix..

--- niflheim/setup.nix
+++ niflheim/setup.nix
@@ -1,33 +1,35 @@
-modules.develop.lua.enable = true;
+{...}: {
+  modules.develop.lua.enable = true;
 
-home.programs.neovim = let
-  customPlugins =
-    pkgs.callPackage "${nvimDir}/niflheim/custom-plugins.nix" pkgs;
-  plugins = pkgs.vimPlugins // customPlugins;
-in {
-  enable = true;
-  package = pkgs.neovim-nightly;
-  extraConfig = builtins.concatStringsSep "\n" [
-    ''
-      lua vim.cmd([[colorscheme ${colorscheme}]])
-      luafile ${builtins.toString "${nvimDir}/niflheim/lua/options.lua"}
-      luafile ${builtins.toString "${nvimDir}/niflheim/lua/keymaps.lua"}
-      lua require('colorizer').setup()
-      lua require('gitsigns').setup()
-      lua require('neogit').setup()
-      lua require('octo').setup()
-      lua require('spectre').setup()
-      lua require('toggleterm').setup()
-      lua require('Comment').setup()
-      lua require('trouble').setup()
-      lua require('nvim-autopairs').setup()
-    ''
-  ];
-  extraPackages = with pkgs; [texlab vale];
-  plugins = pkgs.callPackage "${nvimDir}/niflheim/plugins.nix" plugins;
-};
+  home.programs.neovim = let
+    customPlugins =
+      pkgs.callPackage "${nvimDir}/niflheim/custom-plugins.nix" pkgs;
+    plugins = pkgs.vimPlugins // customPlugins;
+  in {
+    enable = true;
+    package = pkgs.neovim-nightly;
+    extraConfig = builtins.concatStringsSep "\n" [
+      ''
+        lua vim.cmd([[colorscheme ${colorscheme}]])
+        luafile ${builtins.toString "${nvimDir}/niflheim/lua/options.lua"}
+        luafile ${builtins.toString "${nvimDir}/niflheim/lua/keymaps.lua"}
+        lua require('colorizer').setup()
+        lua require('gitsigns').setup()
+        lua require('neogit').setup()
+        lua require('octo').setup()
+        lua require('spectre').setup()
+        lua require('toggleterm').setup()
+        lua require('Comment').setup()
+        lua require('trouble').setup()
+        lua require('nvim-autopairs').setup()
+      ''
+    ];
+    extraPackages = with pkgs; [texlab vale];
+    plugins = pkgs.callPackage "${nvimDir}/niflheim/plugins.nix" plugins;
 
-home.configFile."nvim/lua/my-snippets" = {
-  source = "${nvimDir}/niflheim/my-snippets";
-  recursive = true;
-};
+    home.configFile."nvim/lua/my-snippets" = {
+      source = "${nvimDir}/niflheim/my-snippets";
+      recursive = true;
+    };
+  };
+}
