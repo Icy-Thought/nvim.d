7de2dc08f0c0a7c06e969edbc7c141f6bcea53b7
Icy-Thought <icy-thought@pm.me>
2022-10-16 17:31:39 +0200

    [𝚿] (agasaya): moved neovide-conf to sep. file

--- agasaya/lua/core/init.lua
+++ agasaya/lua/core/init.lua
@@ -55,21 +55,7 @@ end
 
 local neovide_config = function()
     if vim.g.neovide then
-        vim.o.guifont = "VictorMono Nerd Font:h9:b"
-
-        -- general
-        vim.g.neovide_no_idle = true
-        vim.g.neovide_transparency = 0.85
-        vim.g.neovide_cursor_antialiasing = true
-        vim.g.neovide_cursor_trail_length = 0.05
-        vim.g.neovide_cursor_animation_length = 0.03
-
-        -- vfx settings
-        vim.g.neovide_cursor_vfx_mode = "sonicboom"
-        vim.g.neovide_cursor_vfx_opacity = 200.0
-        vim.g.neovide_cursor_vfx_particle_density = 5.0
-        vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
-        vim.g.neovide_cursor_vfx_particle_speed = 20.0
+        require("core.neovide")
     end
 end
 
