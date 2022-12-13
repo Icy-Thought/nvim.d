ad6c3534cd0e83b8722a3ffab14c9ccda26a41b0
Icy-Thought <icy-thought@pm.me>
2022-12-13 16:41:15 +0100

    [𝚫] (agasaya): disable TS when in large files

--- agasaya/lua/modules/editor/config.lua
+++ agasaya/lua/modules/editor/config.lua
@@ -160,6 +160,15 @@ function config.treesitter()
     ts_conf.setup({
         autopairs = { enable = true },
         context_commentstring = { enable = true },
+        -- Disable TS when using larger files
+        disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats =
+                pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
         highlight = {
             enable = true,
             disable = { "vim" },
