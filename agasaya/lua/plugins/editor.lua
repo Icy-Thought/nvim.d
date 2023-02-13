fa40535956a7c09e6316c589b9170befaabee421
Icy-Thought <icy-thought@pm.me>
2023-02-13 16:06:30 +0100

    [𝚫] (agasaya): removed `live-command.nvim`

--- agasaya/lua/plugins/editor.lua
+++ agasaya/lua/plugins/editor.lua
@@ -73,35 +73,6 @@ return {
         config = true,
     },
     {
-        "smjonas/live-command.nvim",
-        event = "BufReadPost",
-        opts = {
-            commands = {
-                G = { cmd = "g" },
-                Norm = { cmd = "norm" },
-                Reg = {
-                    cmd = "norm",
-                    args = function(opts)
-                        return (opts.count == -1 and "" or opts.count)
-                            .. "@"
-                            .. opts.args
-                    end,
-                    range = "",
-                },
-            },
-            defaults = {
-                enable_highlighting = true,
-                inline_highlighting = true,
-                hl_groups = {
-                    insertion = "DiffAdd",
-                    deletion = "DiffDelete",
-                    change = "DiffChange",
-                },
-                debug = false,
-            },
-        },
-    },
-    {
         "brenoprata10/nvim-highlight-colors",
         event = "BufReadPost",
         config = true,
